965cf17c7a5b4c93b6b5cda2b3fbed6b0623a951
Jeff Sacco <jsacco@gamingnationinc.com>
2016-06-09 10:54:35 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,60 @@
+<?php
+
+// This is where your database creds come from
+require '../parameters.php';
+
+
+class myTeams
+{
+     protected $conn;
+     protected $db_host;
+     protected $db_username;
+     protected $db_password;
+     protected $db_database;
+
+
+     public function __construct($host,$username,$password,$database)
+     {
+          $this->db_host = $host;
+          $this->db_username = $username;
+          $this->db_password = $password;
+          $this->db_database = $database;
+     }
+
+     public function connectToDb()
+     {
+
+          $this->conn = new mysqli($this->db_host, $this->db_username, $this->db_password, $this->db_database);
+
+          if ($this->conn->connect_error)
+          {
+               die('Connect Error (' . $this->conn->connect_errno . ') '. $this->conn->connect_error);
+          }
+
+     }
+
+
+
+}
+
+$app = new myTeams($db_host,$db_username,$db_password,$db_database);
+
+$app->connectToDb();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
